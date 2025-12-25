5ef03053c37ffdf52099ed47dc584cd9fe4daf22
Prakash M <mprakash4678@gmail.com>
2025-12-25 11:14:20 +0530

    JavaScript Functions

---
+++ Functions/func.js
@@ -0,0 +1,26 @@
+function greetUser(name, color) {
+  let langauge = "Javascript";
+  let msg = `I am ${name} and I learning ${langauge}, My favorite color is ${color}`;
+  console.log(msg);
+}
+
+greetUser("Prakash", "Red");
+greetUser("Ash", "Yellow");
+
+function add(num1, num2) {
+  return `sum of ${num1} + ${num2} = ${num1 + num2}`;
+}
+
+console.log(add(23, 32));
+
+function mul(n1, n2) {
+  return "Product of " + n1 + " and " + n2 + " is" + " =" + n1 * n2;
+}
+
+console.log(mul(5, 3));
+
+function food(food) {
+  console.log("My fav food is " + food);
+}
+
+food("Biriyani")
