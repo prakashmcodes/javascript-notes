6cd75ae1c96502f72dc69d10e9046f32735f92e6
Prakash M <mprakash4678@gmail.com>
2025-12-25 10:03:00 +0530

    JavaScript Objects

---
+++ Objects/objects.js
@@ -0,0 +1,14 @@
+let person = {
+    name:"Prakash",
+    age:"21",
+    gender:"male",
+    likes:{
+        sports:"cricket",
+        movies:"horror",
+    },
+
+    
+}
+
+
+console.log(person.likes.movies)
