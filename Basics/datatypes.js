e5e5498566b1ab663020e2bd1cfd6880888044d2
Prakash M <mprakash4678@gmail.com>
2025-12-25 10:03:24 +0530

    JavaScript Basics

---
+++ Basics/datatypes.js
@@ -0,0 +1,15 @@
+//Primitive data types
+
+// String = "Set of characters"
+
+//Int = "Numbers"
+
+//Boolean = "True or False"
+
+// Undefined & Null
+
+
+// Reference types
+/* Object
+Array
+Function */
