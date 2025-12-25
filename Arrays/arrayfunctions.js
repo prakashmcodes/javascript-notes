6ab6637cf1e55c914fe91d8b7343b485759252e2
Prakash M <mprakash4678@gmail.com>
2025-12-25 16:46:30 +0530

    Javascript Arrays

---
+++ Arrays/arrayfunctions.js
@@ -0,0 +1,28 @@
+let gadgets = ["Phone","Laptop"]
+
+console.log(gadgets)
+
+gadgets.push("tab") // adds in end
+
+console.log(gadgets)
+
+gadgets.pop() //removes in end
+
+console.log(gadgets);
+
+gadgets.unshift("Headphones"); // adds in start
+
+console.log(gadgets);
+
+gadgets.shift(); // removes in start
+
+console.log(gadgets);
+
+
+gadgets.splice(2, 0, "Powebank") // custom add
+
+console.log(gadgets)
+
+gadgets.splice(0, 2, "Powebank") // custom remove
+
+console.log(gadgets)
---
+++ Arrays/spreadOperator.js
@@ -0,0 +1,7 @@
+let materials = ["pen","pencil","scale"]
+
+let anothers = ["note"]
+
+let studyMaterials = [...materials , ...anothers]
+
+console.log(studyMaterials);
